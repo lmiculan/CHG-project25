94a6c92b6d9429364d9f9198d4fec3c6feb7fb40
lmiculan <leonardo@miculan.org>
2026-05-29 12:29:00 +0200

    Sorted bams and added stats and coverage

--- Tools&pipeline.md
+++ Tools&pipeline.md
@@ -2,19 +2,22 @@
 ## Quality control
 - Samtools 1.21
 - GATK 3.8
-- 
 
-
-# SNV/SNP calling
+## SNV/SNP calling
 - VarScan.v2.3.9
 - snpEff 4.3t
 - Vcftools-v0.1.17-1
 
-# CNV
+## CNV
 - CNVkit
 
-# Tumor heterogenity
+## Tumor heterogenity
 - Clonet2 2.2.1
 
-# Ancestry analysis?
+## Ancestry analysis?
 - Ethseq 3.0.2
+
+# Pipeline
+## Samtools
+- sorting via samtools sort
+- indexing via samtools index
---
+++ scripts/qc&preprocess.sh
@@ -0,0 +1,27 @@
+# View files
+DATA_DIR=data/ogdata
+OUT_DIR=data/bamprocessing
+RESULTS_DIR=data/results
+
+BAMS=$(ls $DATA_DIR/*.bam)
+
+for bam in $BAMS; do
+    basename=$(basename $bam .bam)
+    # echo "Processing $basename.bam..."
+    # samtools view -H -f 4 -F 8 $basename  | head -n 20
+
+    # ## Sorting and indexing
+    # echo "Sorting and indexing $basename.bam..."
+    # samtools sort -o $OUT_DIR/$basename.sorted.bam -T $OUT_DIR/$basename.tmp -@ 4 $DATA_DIR/$basename.bam
+    # samtools index $OUT_DIR/$basename.sorted.bam
+
+    # Summary statistics
+    echo "Generating summary statistics for $basename.bam..."
+    samtools flagstat $OUT_DIR/$basename.sorted.bam > $RESULTS_DIR/$basename.flagstat.txt
+
+    ## Single base depth and coverage
+    echo "Calculating single base coverage for $basename.bam..."
+    samtools bedcov $DATA_DIR/Captured_Regions.bed $OUT_DIR/$basename.sorted.bam > $RESULTS_DIR/$basename.bedcov.txt
+    samtools depth  $DATA_DIR/Captured_Regions.bed $OUT_DIR/$basename.sorted.bam > $RESULTS_DIR/$basename.beddepth.txt
+done
+
