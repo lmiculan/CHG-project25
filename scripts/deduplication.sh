f9608df3379cfacf46ca78e8ffba6f6e76a87540
lmiculan <leonardo@miculan.org>
2026-06-29 15:10:23 +0200

    Deudulication done

--- scripts/deduplication.sh
+++ scripts/deduplication.sh
@@ -1,18 +1,19 @@
 # Script for managing PCR duplicate reads in BAM files using Picard tools.
-JAVA8=/usr/lib/jvm/java-8-openjdk/bin/java
 REF=data/annotations/human_g1k_v37.fasta
-BAMS=data/bamprocessing/*.recal.bam
+BAMS=data/bamprocessing/recal/*.recal.bam
 PICARD=~/bin/picard.jar
 
 mkdir -p data/bamprocessing/dedup
 
 for bam in $BAMS; do
-    base=$(basename $bam .recal.bam)
+    base=$(basename $bam .bam)
     echo "Processing $base"
-    $JAVA8 -jar /usr/share/java/picard.jar MarkDuplicates \
+    java -jar $PICARD MarkDuplicates \
         I=$bam \
         O=data/bamprocessing/dedup/$base.dedup.bam \
         M=data/bamprocessing/dedup/$base.metrics.txt \
         REMOVE_DUPLICATES=true \
         ASSUME_SORTED=true
+
+    samtools index data/bamprocessing/dedup/$base.dedup.bam
 done
