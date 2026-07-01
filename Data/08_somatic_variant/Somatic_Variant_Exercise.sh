a5e7f7fca431dd70e6918ebddcf4ae71e0a2f034
giobunny-git <giovanni.linussio@studenti.unitn.it>
2026-07-01 11:38:19 +0200

    08 script

--- Data/08_somatic_variant/Somatic_Variant_Exercise.sh
+++ Data/08_somatic_variant/Somatic_Variant_Exercise.sh
@@ -1,21 +1,20 @@
-cd Data
-
 #### Somatic point mutations
 
 ### preparing pileups
-samtools mpileup -q 1 -f ../../../annotations/human_g1k_v37.fasta Normal_chr13_chr20.sorted.bam > Normal_chr13_chr20.sorted.pileup
-samtools mpileup -q 1 -f ../../../annotations/human_g1k_v37.fasta Tumor_chr13_chr20.sorted.bam > Tumor_chr13_chr20.sorted.pileup
+samtools mpileup -q 1 -f ../../../../../annotations/human_g1k_v37.fasta ../04_deduplication/Control.sorted.realigned.recalibrated.dedup.bam > Control.pileup
+samtools mpileup -q 1 -f ../../../../../annotations/human_g1k_v37.fasta ../04_deduplication/Tumor.sorted.realigned.recalibrated.dedup.bam > Tumor.pileup
 
 ### running Varscan2
-java -jar ../../../tools/VarScan.v2.3.9.jar somatic Normal_chr13_chr20.sorted.pileup Tumor_chr13_chr20.sorted.pileup --output-snp somatic.pm --output-indel somatic.indel --output-vcf 1
+java -jar ../../../../../tools/VarScan.v2.3.9.jar somatic Control.pileup Tumor.pileup --output-snp somatic.pm --output-indel somatic.indel --output-vcf 1
+# gave me 0 hits, stopped here :(
 
 ## Annotation
-java -Xmx4g -jar ../../../tools/snpEff/SnpSift.jar Annotate ../../../annotations/hapmap_3.3.b37.vcf  somatic.pm.vcf > somatic.pm.vcf.hapmap_ann.vcf
+java -Xmx4g -jar ../../../../../tools/snpEff/SnpSift.jar Annotate ../../../../../annotations/hapmap_3.3.b37.vcf  somatic.pm.vcf > somatic.pm.vcf.hapmap_ann.vcf
 
 ## Filtering vcf
-cat somatic.pm.vcf.hapmap_ann.vcf | java -Xmx4g -jar ../../../tools/snpEff/SnpSift.jar filter "(POS = 1896100)"
-cat somatic.pm.vcf.hapmap_ann.vcf | java -Xmx4g -jar ../../../tools/snpEff/SnpSift.jar filter "(exists ID) & ( ID =~ 'rs' )" > somatic.pm.onlySNPs.vcf
-cat somatic.pm.vcf.hapmap_ann.vcf | java -Xmx4g -jar ../../../tools/snpEff/SnpSift.jar filter "!(exists ID) & !( ID =~ 'rs' )" > somatic.pm.noSNPs.vcf
+cat somatic.pm.vcf.hapmap_ann.vcf | java -Xmx4g -jar ../../../../../tools/snpEff/SnpSift.jar filter "(POS = 1896100)"
+cat somatic.pm.vcf.hapmap_ann.vcf | java -Xmx4g -jar ../../../../../tools/snpEff/SnpSift.jar filter "(exists ID) & ( ID =~ 'rs' )" > somatic.pm.onlySNPs.vcf
+cat somatic.pm.vcf.hapmap_ann.vcf | java -Xmx4g -jar ../../../../../tools/snpEff/SnpSift.jar filter "!(exists ID) & !( ID =~ 'rs' )" > somatic.pm.noSNPs.vcf
 
 
 
