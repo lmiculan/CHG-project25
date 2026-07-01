74f3d1c63d0d904297e95afab88aa93fbe06bbb9
LaxacoM <odranoel.lm@gmail.com>
2026-07-01 18:14:32 +0000

    Fixed GVC and added BED targetd GVC

--- scripts/005_scnv_varscan_cnvkit.sh
+++ scripts/005_scnv_varscan_cnvkit.sh
@@ -1,30 +1,31 @@
 # SCNV with VarScan
-BAMS=data/bamprocessing/dedup/*.dedup.bam
+NORMAL=data/bamprocessing/dedup/Control.sorted.realigned.recal.bam
+TUMOR=data/bamprocessing/dedup/Tumor.sorted.realigned.recal.bam
 REF=data/annotations/human_g1k_v37.fasta
 VARSCAN=~/bin/VarScan.v2.3.9.jar
 OUTDIR=results/scnv_varscan
 
 mkdir -p $OUTDIR
 
-echo "Processing $BAMS"
+# echo "Processing bams"
 
-samtools mpileup -B -q 1 -f "$REF" $BAMS | \
-    java -Xmx4g -jar $VARSCAN copynumber \
-    - \
-    "$OUTDIR/SCNA" \
-    --mpileup 1
+# samtools mpileup -B -q 1 -f "$REF" "$NORMAL" "$TUMOR" | \
+#     java -Xmx4g -jar $VARSCAN copynumber \
+#     - \
+#     "$OUTDIR/SCNA" \
+#     --mpileup 1
 
-echo "Calling copy number variants with VarScan"
+# echo "Calling copy number variants with VarScan"
 
-java -jar $VARSCAN copyCaller $OUTDIR/SCNA.copynumber --output-file $OUTDIR/SCNA.copynumber.called
+# java -jar $VARSCAN copyCaller $OUTDIR/SCNA.copynumber --output-file $OUTDIR/SCNA.copynumber.called
 
-##CBS segmentation with CNVkit
-echo "Segmenting with CNVkit"
+# ##CBS segmentation with CNVkit
+# echo "Segmenting with CNVkit"
 
-# Correct formatted headers for CNVkit (replace first row names with 'chromosome', 'start', 'end', 'gene', 'log2')
-# Putting . as gene name for now, since VarScan does not provide gene names in the output
-awk 'BEGIN {FS="\t"; OFS="\t"; print "chromosome", "start", "end", "gene", "log2", "weight"}
-    NR>1 {print $1, $2, $3, ".", $7, "1.0"}' $OUTDIR/SCNA.copynumber.called > $OUTDIR/SCNA.copynumber.called.formatted.cns
+# # Correct formatted headers for CNVkit (replace first row names with 'chromosome', 'start', 'end', 'gene', 'log2')
+# # Putting . as gene name for now, since VarScan does not provide gene names in the output
+# awk 'BEGIN {FS="\t"; OFS="\t"; print "chromosome", "start", "end", "gene", "log2", "weight"}
+#     NR>1 {print $1, $2, $3, ".", $7, "1.0"}' $OUTDIR/SCNA.copynumber.called > $OUTDIR/SCNA.copynumber.called.formatted.cns
 
 cnvkit.py segment $OUTDIR/SCNA.copynumber.called.formatted.cns -o $OUTDIR/SCNA.copynumber.called.segmented.cns
 
--- scripts/006_gvc_gatk.sh
+++ scripts/006_gvc_gatk.sh
@@ -1,55 +1,129 @@
 # Germline variant calling with GATK
+set -e
 JAVA8=/usr/lib/jvm/java-8-openjdk/bin/java
 GATK=~/bin/GenomeAnalysisTK.jar
-BAMS=data/bamprocessing/dedup/*.dedup.bam
+BAMS=data/bamprocessing/dedup/Control.*.dedup.bam
 REF=data/annotations/human_g1k_v37.fasta
+BED=data/ogdata/Captured_Regions.bed
 OUTDIR=results/gcv_gatk
 
 mkdir -p $OUTDIR
 
-#SCV with GATK UnifiedGenotyper
+#SCV with GATK UnifiedGenotyper FULL GENOME
 
-for BAM in $BAMS; do
-    basename=$(basename $BAM .sorted.realigned.recal.dedup.bam)
-    echo "Processing $basename"
+# for BAM in $BAMS; do
+#     basename=$(basename $BAM .sorted.realigned.recal.dedup.bam)
+#     echo "Processing $basename"
 
-    $JAVA8 -Xmx4g -jar $GATK \
-        -T UnifiedGenotyper \
-        -R $REF \
-        -I $BAM \
-        -o $OUTDIR/{$basename}_GVC.vcf \
-        --genotype_likelihoods_model BOTH \
-        --output_mode EMIT_VARIANTS_ONLY
+#     $JAVA8 -Xmx4g -jar $GATK \
+#         -T UnifiedGenotyper \
+#         -R $REF \
+#         -I $BAM \
+#         -o $OUTDIR/$basename\_GVC.vcf \
+#         --genotype_likelihoods_model BOTH \
+#         --output_mode EMIT_VARIANTS_ONLY
 
-    # Filtering out low quality variants with vcftools
-    vcftools --minQ 20 \
-             --max-meanDP 2000 \
-             --min-meanDP 5 \
-             --remove-indels \
-             --vcf  $OUTDIR/$basename\_GVC.vcf \
-             --out $OUTDIR/$basename\_GVC_filtered_snps.bcf \
-             --recode \
-             --recode-INFO-all
+#     # Filtering out low quality variants with vcftools
+#     vcftools --minQ 20 \
+#              --max-meanDP 2000 \
+#              --min-meanDP 5 \
+#              --remove-indels \
+#              --vcf  $OUTDIR/$basename\_GVC.vcf \
+#              --out $OUTDIR/$basename\_GVC_filtered_snps.bcf \
+#              --recode \
+#              --recode-INFO-all
 
-    vcftools --minQ 20 \
-             --max-meanDP 2000 \
-             --min-meanDP 5 \
-             --keep-only-indels \
-             --vcf  $OUTDIR/$basename\_GVC.vcf \
-             --out $OUTDIR/$basename\_GVC_filtered_indels.bcf \
-             --recode \
-             --recode-INFO-all
-done
+#     vcftools --minQ 20 \
+#              --max-meanDP 2000 \
+#              --min-meanDP 5 \
+#              --keep-only-indels \
+#              --vcf  $OUTDIR/$basename\_GVC.vcf \
+#              --out $OUTDIR/$basename\_GVC_filtered_indels.bcf \
+#              --recode \
+#              --recode-INFO-all
+# done
+
+# Checking not maching chromosomes between VCFs
+# echo "Checking not maching chromosomes between VCFs"
+# awk 'NR==FNR{a[$1];next} !($1 in a)' $OUTDIR/Control_GVC_filtered_snps.bcf.recode.vcf $OUTDIR/Tumor_GVC_filtered_snps.bcf.recode.vcf > $OUTDIR/Not_matching_chromosomes.txt
+# awk 'NR==FNR{a[$1];next} !($1 in a)' $OUTDIR/Control_GVC_filtered_indels.bcf.recode.vcf $OUTDIR/Tumor_GVC_filtered_indels.bcf.recode.vcf > $OUTDIR/Not_matching_chromosomes_indels.txt
+
+
+# # Comparing VCFs to check for common variants across samples
+# echo "Comparing SNPs VCFs to check for common variants across samples"
+
+# vcftools --vcf $OUTDIR/Control_GVC_filtered_snps.bcf.recode.vcf \
+#          --diff $OUTDIR/Tumor_GVC_filtered_snps.bcf.recode.vcf \
+#          --out $OUTDIR/Common_Variants \
+#          --diff-site \
+#          --not-chr 4 --not-chr 13 --not-chr GL000198.1 --not-chr GL000193.1
+
+# # Comparing VCFs to check for common variants across samples
+# echo "Comparing indel VCFs to check for common variants across samples"
+
+# vcftools --vcf $OUTDIR/Control_GVC_filtered_indels.bcf.recode.vcf \
+#          --diff $OUTDIR/Tumor_GVC_filtered_indels.bcf.recode.vcf \
+#          --out $OUTDIR/Common_Indels \
+#          --diff-site \
+#          --not-chr 3 --not-chr GL000203.1
+
+OUTDIR=results/gvc_gatk_bed
+
+mkdir -p $OUTDIR
+
+# Germline variant calling with GATK BED TARGETED
+# for BAM in $BAMS; do
+#     basename=$(basename $BAM .sorted.realigned.recal.dedup.bam)
+#     echo "Processing $basename"
+
+#     $JAVA8 -Xmx4g -jar $GATK \
+#         -T UnifiedGenotyper \
+#         -L $BED \
+#         -R $REF \
+#         -I $BAM \
+#         -o $OUTDIR/$basename\_GVC.vcf \
+#         --genotype_likelihoods_model BOTH \
+#         --output_mode EMIT_VARIANTS_ONLY
+
+#    # Filtering out low quality variants with vcftools
+#     vcftools --minQ 20 \
+#              --max-meanDP 2000 \
+#              --min-meanDP 5 \
+#              --remove-indels \
+#              --vcf  $OUTDIR/$basename\_GVC.vcf \
+#              --out $OUTDIR/$basename\_GVC_filtered_snps.bcf \
+#              --recode \
+#              --recode-INFO-all
+
+#     vcftools --minQ 20 \
+#              --max-meanDP 2000 \
+#              --min-meanDP 5 \
+#              --keep-only-indels \
+#              --vcf  $OUTDIR/$basename\_GVC.vcf \
+#              --out $OUTDIR/$basename\_GVC_filtered_indels.bcf \
+#              --recode \
+#              --recode-INFO-all
+# done
+
+#Checking not maching chromosomes between VCFs
+echo "Checking not maching chromosomes between VCFs"
+awk 'NR==FNR{a[$1];next} !($1 in a)' $OUTDIR/Control_GVC_filtered_snps.bcf.recode.vcf $OUTDIR/Tumor_GVC_filtered_snps.bcf.recode.vcf > $OUTDIR/Not_matching_chromosomes.txt
+awk 'NR==FNR{a[$1];next} !($1 in a)' $OUTDIR/Control_GVC_filtered_indels.bcf.recode.vcf $OUTDIR/Tumor_GVC_filtered_indels.bcf.recode.vcf > $OUTDIR/Not_matching_chromosomes_indels.txt
+
 
 # Comparing VCFs to check for common variants across samples
-echo "Comparing VCFs to check for common variants across samples"
+echo "Comparing SNPs VCFs to check for common variants across samples"
 
-vcftools --vcf $OUTDIR/Control_GVC_filtered.bcf.recode.vcf \
-         --diff $OUTDIR/Tumor_GVC_filtered.bcf.recode.vcf \
-         --out $OUTDIR/Common_Variants.vcf \
-         --diff-site \
-        #  --chr 1 --chr 2 --chr 3 --chr 5 --chr 6 --chr 7 --chr 8 --chr 9 \
-        #  --chr 10 --chr 11 --chr 12 --chr 14 --chr 15 --chr 16 --chr 17 \
-        #  --chr 18 --chr 19 --chr 20 --chr 21 --chr 22 --chr X --chr Y \
+vcftools --vcf $OUTDIR/Control_GVC_filtered_snps.bcf.recode.vcf \
+         --diff $OUTDIR/Tumor_GVC_filtered_snps.bcf.recode.vcf \
+         --out $OUTDIR/Common_Variants \
+         --diff-site
 
+# Comparing VCFs to check for common variants across samples
+echo "Comparing indel VCFs to check for common variants across samples"
+
+vcftools --vcf $OUTDIR/Control_GVC_filtered_indels.bcf.recode.vcf \
+         --diff $OUTDIR/Tumor_GVC_filtered_indels.bcf.recode.vcf \
+         --out $OUTDIR/Common_Indels \
+         --diff-site
 
